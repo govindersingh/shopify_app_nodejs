3cb23f9f922c98d2a7bc47565b9bdaa15162f500
Govinder singh <govindersingh05@gmail.com>
2023-02-07 07:34:07 +0530

    complete the test bridge app.

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -29,40 +29,8 @@ export default function HomePage() {
             >
               <Stack.Item fill>
                 <TextContainer spacing="loose">
-                  <Heading>Nice work on building a Shopify app 🎉</Heading>
-                  <p>
-                    Your app is ready to explore! It contains everything you
-                    need to get started including the{" "}
-                    <Link url="https://polaris.shopify.com/" external>
-                      Polaris design system
-                    </Link>
-                    ,{" "}
-                    <Link url="https://shopify.dev/api/admin-graphql" external>
-                      Shopify Admin API
-                    </Link>
-                    , and{" "}
-                    <Link
-                      url="https://shopify.dev/apps/tools/app-bridge"
-                      external
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    UI library and components.
-                  </p>
-                  <p>
-                    Ready to go? Start populating your app with some sample
-                    products to view and test in your store.{" "}
-                  </p>
-                  <p>
-                    Learn more about building out your app in{" "}
-                    <Link
-                      url="https://shopify.dev/apps/getting-started/add-functionality"
-                      external
-                    >
-                      this Shopify tutorial
-                    </Link>{" "}
-                    📚{" "}
-                  </p>
+                  <Heading>Welcome to Shopify app</Heading>
+                  <p>{new URLSearchParams(location.search).get("shop")}</p>
                 </TextContainer>
               </Stack.Item>
               <Stack.Item>
@@ -77,9 +45,9 @@ export default function HomePage() {
             </Stack>
           </Card>
         </Layout.Section>
-        <Layout.Section>
+        {/* <Layout.Section>
           <ProductsCard />
-        </Layout.Section>
+        </Layout.Section> */}
       </Layout>
     </Page>
   );
